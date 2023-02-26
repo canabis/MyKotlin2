3000b6e4ce92142b20519be30480a8cced12b0e6
canabis <canabis.tw@gmail.com>
2023-02-26 23:39:48 +0800

    s

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+//    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    //println("Program arguments: ${args.joinToString()}")
+
+//    Human().hello()
+
+    val h=Human();
+    h.hello()
+
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin!!")
+    }
+}
